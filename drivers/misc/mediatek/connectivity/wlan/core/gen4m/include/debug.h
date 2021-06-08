d937c8ca78f66d161e227bc52bc44567e1fcc62e
Vaisakh Murali <mvaisakh@statixos.com>
2021-06-08 12:42:28 +0530

    drivers: mtk-wlan-gen4m: Disable all debug logs by default
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/include/debug.h
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/include/debug.h
@@ -71,7 +71,7 @@
 #define BUILD_QA_DBG 0
 #endif
 
-#define DBG_DISABLE_ALL_LOG             0
+#define DBG_DISABLE_ALL_LOG             1
 
 /*******************************************************************************
  *                    E X T E R N A L   R E F E R E N C E S
