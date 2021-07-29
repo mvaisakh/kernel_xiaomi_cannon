ad90807dce4c9f5342ea90e568c7efa4ad34224e
Vaisakh Murali <mvaisakh@statixos.com>
2021-07-29 16:51:03 +0530

    include: Do not always inline ksm_flock
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- include/linux/keyslot-manager.h
+++ include/linux/keyslot-manager.h
@@ -98,7 +98,7 @@ int keyslot_manager_derive_raw_secret(struct keyslot_manager *ksm,
 				      unsigned int wrapped_key_size,
 				      u8 *secret, unsigned int secret_size);
 
-inline void ksm_flock(struct keyslot_manager *ksm, unsigned int flags);
+void ksm_flock(struct keyslot_manager *ksm, unsigned int flags);
 
 #endif /* CONFIG_BLK_INLINE_ENCRYPTION */
 
