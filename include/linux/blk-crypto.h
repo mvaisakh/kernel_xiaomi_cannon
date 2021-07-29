4a05dd36e604812774716cc3e11ca09299ff364f
Vaisakh Murali <mvaisakh@statixos.com>
2021-07-29 16:50:13 +0530

    include: blk-crypto: Do not always inline blk_crypto_flock
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- include/linux/blk-crypto.h
+++ include/linux/blk-crypto.h
@@ -32,7 +32,7 @@ int blk_crypto_start_using_mode(enum blk_crypto_mode_num crypto_mode,
 int blk_crypto_evict_key(struct request_queue *q,
 			 const struct blk_crypto_key *key);
 
-inline void blk_crypto_flock(struct keyslot_manager *ksm, unsigned int flags);
+void blk_crypto_flock(struct keyslot_manager *ksm, unsigned int flags);
 
 
 #else /* CONFIG_BLK_INLINE_ENCRYPTION */
