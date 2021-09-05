2ad7043d69a8a9276a00b608ce2b3461c7ee00dd
Vaisakh Murali <mvaisakh@statixos.com>
2021-09-05 22:21:28 +0530

    script: Switch to EvaGCC
    Giga Chad Compiler
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- cannon-build.sh
+++ cannon-build.sh
@@ -66,7 +66,7 @@ DEFCONFIG=cannon_user_defconfig
 
 # Specify compiler. 
 # 'clang' or 'gcc'
-COMPILER=clang
+COMPILER=gcc
 
 # Clean source prior building. 1 is NO(default) | 0 is YES
 INCREMENTAL=1
@@ -167,8 +167,8 @@ DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
 	if [ $COMPILER = "gcc" ]
 	then
 		msg "|| Cloning GCC 9.3.0 baremetal ||"
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git gcc64
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git gcc32
+		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git gcc64 -b gcc-new
+		git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git gcc32 -b gcc-new
 		GCC64_DIR=$KERNEL_DIR/gcc64
 		GCC32_DIR=$KERNEL_DIR/gcc32
 	fi
