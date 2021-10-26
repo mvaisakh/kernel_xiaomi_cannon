cd5591c00eac2b36555ddb63c39b1ba26946e3c0
Vaisakh Murali <mvaisakh@statixos.com>
2021-10-26 17:18:46 +0530

    drivers/gpu: drm: cannon: Shut up with massive obscure logspam
    This is panel debugging used for setting GPU parameters and display
    positions through the dmesg. But this has no significance for a
    normal user or even a custom kernel developer.
    
    Shut it up by setting up proper printing API.
    
    Silences this massive logspam:
    [674:composer@2.1-se]P+/0/L13/idx3913/fd47
    [674:composer@2.1-se]P+/P0/L0/id3873/fd135/hnd0xe88600ab-0xfffffff918a44000
    [674:composer@2.1-se]P+/P0/L1/id3790/fd139/hnd0xfcb7bff2-0xfffffff8834d4c00
    [674:composer@2.1-se]P+/P0/L2/id3710/fd142/hnd0x2ecc4b88-0xfffffff8a596e600
    [674:composer@2.1-se]P+/P0/L3/id3770/fd145/hnd0x62b993ad-0xfffffff9450cf800
    [674:composer@2.1-se]P+/P0/L4/id3769/fd148/hnd0x1d9c0fad-0xfffffff91000e000
    [674:composer@2.1-se]P+/P0/L5/id1443/fd151/hnd0x9d71e4e7-0xfffffff9450cc400
    [781:OverlayEngine_0]S+/PL0/e1/id3873/mva0xfac76000/size0x009eb000/S0
    [781:OverlayEngine_0]S+/PL1/e1/id3790/mva0xfa176000/size0x00079000/S0
    [781:OverlayEngine_0]S+/PL2/e1/id3710/mva0xf84ca000/size0x009eb000/S0
    [781:OverlayEngine_0]S+/PL3/e1/id3770/mva0xfca37000/size0x0009c000/S0
    [781:OverlayEngine_0]S+/PL4/e1/id3769/mva0xfd7a7000/size0x0009c000/S0
    [781:OverlayEngine_0]S+/PL5/e1/id1443/mva0xfd843000/size0x0009c000/S0
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- drivers/gpu/drm/mediatek_cannon/mtk_log.h
+++ drivers/gpu/drm/mediatek_cannon/mtk_log.h
@@ -37,17 +37,17 @@ int mtk_dprec_logger_pr(unsigned int type, char *fmt, ...);
 	do {                                                                   \
 		mtk_dprec_logger_pr(DPREC_LOGGER_DEBUG, fmt, ##arg);           \
 		if (g_mobile_log)                                              \
-			pr_info(pr_fmt(fmt), ##arg);     \
+			pr_debug(pr_fmt(fmt), ##arg);     \
 	} while (0)
 #define DDPDSIINFO(fmt, arg...)                                                   \
 	do {                                                                   \
 		mtk_dprec_logger_pr(DPREC_LOGGER_DEBUG, fmt, ##arg);           \
 		if (g_dsi_log)                                              \
-			pr_info(pr_fmt(fmt), ##arg);     \
+			pr_debug(pr_fmt(fmt), ##arg);     \
 	} while (0)
 
 #define DDPFUNC(fmt, arg...)		\
-	pr_info("[%s line:%d]"pr_fmt(fmt), __func__, __LINE__, ##arg)
+	pr_debug("[%s line:%d]"pr_fmt(fmt), __func__, __LINE__, ##arg)
 
 #define DDPDBG(fmt, arg...)                                                    \
 	do {                                                                   \
@@ -55,27 +55,27 @@ int mtk_dprec_logger_pr(unsigned int type, char *fmt, ...);
 			break;                                                 \
 		mtk_dprec_logger_pr(DPREC_LOGGER_DEBUG, fmt, ##arg);           \
 		if (g_mobile_log)                                              \
-			pr_info(pr_fmt(fmt), ##arg);     \
+			pr_debug(pr_fmt(fmt), ##arg);     \
 	} while (0)
 
 #define DDPMSG(fmt, arg...)                                                    \
 	do {                                                                   \
 		mtk_dprec_logger_pr(DPREC_LOGGER_DEBUG, fmt, ##arg);           \
-		pr_info(pr_fmt(fmt), ##arg);             \
+		pr_debug(pr_fmt(fmt), ##arg);             \
 	} while (0)
 
 #define DDPDUMP(fmt, arg...)                                                   \
 	do {                                                                   \
 		mtk_dprec_logger_pr(DPREC_LOGGER_DUMP, fmt, ##arg);            \
 		if (g_mobile_log)                                              \
-			pr_info(pr_fmt(fmt), ##arg);     \
+			pr_debug(pr_fmt(fmt), ##arg);     \
 	} while (0)
 
 #define DDPFENCE(fmt, arg...)                                                  \
 	do {                                                                   \
 		mtk_dprec_logger_pr(DPREC_LOGGER_DEBUG, fmt, ##arg);           \
 		if (1)                                               \
-			pr_info(pr_fmt(fmt), ##arg);     \
+			pr_debug(pr_fmt(fmt), ##arg);     \
 	} while (0)
 
 #define DDPPR_ERR(fmt, arg...)                                                 \
