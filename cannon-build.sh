e7112c290e46a46e4a096148480b3f8518dd2462
Vaisakh Murali <mvaisakh@statixos.com>
2021-11-25 19:25:15 +0600

    build-script: Clone gcc with single-branch
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- cannon-build.sh
+++ cannon-build.sh
@@ -168,8 +168,8 @@ DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
 	if [ $COMPILER = "gcc" ]
 	then
 		msg "|| Cloning GCC 9.3.0 baremetal ||"
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git gcc64
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git gcc32
+		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git gcc64 --single-branch
+		git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git gcc32 --single-branch
 		GCC64_DIR=$KERNEL_DIR/gcc64
 		GCC32_DIR=$KERNEL_DIR/gcc32
 	fi
