0b5d4fd8e288cf997287b9b230fc3bf562a14e63
Vaisakh Murali <mvaisakh@statixos.com>
2022-02-03 11:49:27 +0600

    Revert "[TO-DROP] kernel: Import Xiaomi+MTK changes"
    This reverts commit 9cdc3e09809e56ee10494abb011acdc8ca7170e3.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -2,7 +2,6 @@
  * Generic ring buffer
  *
  * Copyright (C) 2008 Steven Rostedt <srostedt@redhat.com>
- * Copyright (C) 2021 XiaoMi, Inc.
  */
 #include <linux/trace_events.h>
 #include <linux/ring_buffer.h>
@@ -4286,7 +4285,6 @@ void ring_buffer_reset_cpu(struct ring_buffer *buffer, int cpu)
 	/* prevent another thread from changing buffer sizes */
 	mutex_lock(&buffer->mutex);
 
-	mutex_lock(&buffer->mutex);
 	atomic_inc(&buffer->resize_disabled);
 	atomic_inc(&cpu_buffer->record_disabled);
 
@@ -4309,7 +4307,6 @@ void ring_buffer_reset_cpu(struct ring_buffer *buffer, int cpu)
 
 	atomic_dec(&cpu_buffer->record_disabled);
 	atomic_dec(&buffer->resize_disabled);
-	mutex_unlock(&buffer->mutex);
 }
 EXPORT_SYMBOL_GPL(ring_buffer_reset_cpu);
 
